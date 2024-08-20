93c2a152574582350d24461b6ca3c35109cea0bc
hairb <hairb@TYLERHP>
2024-08-20 19:11:52 +0400

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Data() {
+            Dictionary<String,String> keyValuePairs = new Dictionary<String,String>();
+            keyValuePairs.Add("1", "foo");
+            keyValuePairs.Add("2", "bar");
+            keyValuePairs.Add("3", "baz");
+            keyValuePairs.Add("4", "gaaa");
+            ViewBag.Data = keyValuePairs;
+            return View();
+        }
+    }
+}
